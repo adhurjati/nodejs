fa89ba1b3291dc43b7ace295929021928085254e
Aparna Dhurjati <aparnasudha@yahoo.com>
2018-12-26 19:27:09 -0500

    express example with routes and middleware with morgan and body-parser modules

---
+++ express-example/server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const morgan = require('morgan') 
+
+var app = express()
+
+app.use(bodyparser.json())
+app.use(morgan('dev'))
+
+app.use((req,res,next) => {
+ console.log(`${req.method} ${req.url}`)
+ if (req.query.api_key){
+     next()
+ }else{
+    next(new Error('Ooops!'))
+ }
+})
+
+const myMiddleware = (req, res, next) => {
+    console.log('in myMiddleware function. Req Method is ' + req.method)
+    next()
+}
+
+app.use(myMiddleware)
+
+app.use((error, req, res, next) => {
+    res.status(500).send(error)
+})
+
+app.get('/', (req,res,next)=>{
+    console.log('in inline middleware')
+    next()
+},(req,res) => {
+    res.send({msg: 'hello world'})
+})
+
+app.get('/accounts', (req,res) => {
+    res.send({msg: 'accounts'})
+})
+
+app.post('/transactions', (req,res) => {
+    console.log(req.body)
+    res.send({msg: 'transactions'})
+})
+
+app.listen(3000)
